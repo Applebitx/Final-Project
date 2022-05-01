99e68e1e78619a9db07c272594109fc9e1cfd640
Applebitx <applebitx@gmail.com>
2022-05-01 03:29:56 +0300

    first impl CL

---
+++ Final Project/View/WeatherCell/MainWeatherTableViewCell/MainWeatherTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MainWeatherTableViewCell.swift
+//  Final Project
+//
+//  Created by Денис Сторожик on 30.04.2022.
+//
+
+import UIKit
+
+class MainWeatherTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
